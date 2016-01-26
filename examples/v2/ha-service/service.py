a1921434a9c24359c0df6917e19eafa69328ea56
bmelville <bmelville@google.com>
2016-01-26 14:47:32 -0800

    Refactoring some pieces in ha-service example to make them usable from other examples. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=113098702

--- examples/v2/ha-service/service.py
+++ examples/v2/ha-service/service.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc. All rights reserved.
+# Copyright 2016 Google Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,34 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Creates autoscaled, network LB IGM running specified docker image."""
-
-# Defaults
-SIZE_KEY = 'size'
-DEFAULT_SIZE = 1
-
-MAX_SIZE_KEY = 'maxSize'
-DEFAULT_MAX_SIZE = 1
-
-CONTAINER_IMAGE_KEY = 'containerImage'
-DEFAULT_CONTAINER_IMAGE = 'container-vm-v20151103'
-
-DOCKER_ENV_KEY = 'dockerEnv'
-DEFAULT_DOCKER_ENV = {}
+"""Creates autoscaled IGM running specified docker image."""
 
 
 def GenerateConfig(context):
   """Generate YAML resource configuration."""
-
-  # Set up some defaults if the user didn't provide any
-  if SIZE_KEY not in context.properties:
-    context.properties[SIZE_KEY] = DEFAULT_SIZE
-  if MAX_SIZE_KEY not in context.properties:
-    context.properties[MAX_SIZE_KEY] = DEFAULT_MAX_SIZE
-  if CONTAINER_IMAGE_KEY not in context.properties:
-    context.properties[CONTAINER_IMAGE_KEY] = DEFAULT_CONTAINER_IMAGE
-  if DOCKER_ENV_KEY not in context.properties:
-    context.properties[DOCKER_ENV_KEY] = DEFAULT_DOCKER_ENV
 
   name = context.env['name']
   port = context.properties['port']
@@ -49,36 +26,54 @@ def GenerateConfig(context):
   igm_name = name + '-igm'
   it_name = name + '-it'
 
-  resources = [{
+  it = {
       'name': it_name,
       'type': 'container_instance_template.py',
       'properties': {
-          CONTAINER_IMAGE_KEY: context.properties[CONTAINER_IMAGE_KEY],
-          DOCKER_ENV_KEY: context.properties[DOCKER_ENV_KEY],
+          'containerImage': context.properties['containerImage'],
+          'dockerEnv': context.properties['dockerEnv'],
           'dockerImage': context.properties['dockerImage'],
           'port': port
       }
-  }, {
+  }
+
+  igm = {
       'name': igm_name,
       'type': 'compute.v1.instanceGroupManager',
       'properties': {
           'baseInstanceName': name + '-instance',
           'instanceTemplate': '$(ref.' + it_name + '.selfLink)',
-          'targetSize': context.properties[SIZE_KEY],
-          'targetPools': ['$(ref.' + target_pool + '.selfLink)'],
+          'targetSize': context.properties['minSize'],
           'zone': zone
       }
-  }, {
+  }
+
+  # Set target pool if one was provided.
+  if target_pool:
+    igm['properties']['targetPools'] = ['$(ref.' + target_pool + '.selfLink)']
+
+  autoscaler = {
       'name': name + '-as',
       'type': 'compute.v1.autoscaler',
       'properties': {
           'autoscalingPolicy': {
-              'maxNumReplicas': context.properties[MAX_SIZE_KEY]
+              'minNumReplicas': context.properties['minSize'],
+              'maxNumReplicas': context.properties['maxSize']
           },
           'target': '$(ref.' + igm_name + '.selfLink)',
           'zone': zone
       }
-  }]
+  }
 
-  return {'resources': resources}
+  return {
+      'resources': [
+          it,
+          igm,
+          autoscaler
+      ],
+      'outputs': [{
+          'name': 'group',
+          'value': '$(ref.' + igm_name + '.group)'
+      }]
+  }
 
