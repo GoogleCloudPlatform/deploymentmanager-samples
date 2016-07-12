eac57086393c23123b74ab92a58eaa450d99b288
yyxia <yyxia@google.com>
2016-07-12 14:05:57 -0700

    add configuration samples for quick start and step by step guide under public documentation
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=127239754

---
+++ examples/v2/container_vm/README.md
@@ -0,0 +1,9 @@
+# Create Container Deployment
+
+## Overview
+
+This is a [Google Cloud Deployment Manager]
+(https://cloud.google.com/deployment-manager/step-by-step-guide/create-container-deployment)
+configuration example that creates a VM using containers. To use it, replace
+ZONE_TO_RUN with specific zone in container_vm.yaml file. This example can use
+either Jinja or Python templates.
--- examples/v2/container_vm/python/container_vm.py
+++ examples/v2/container_vm/python/container_vm.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc. All rights reserved.
+# Copyright 2016 Google Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Creates a Container VM with the provided Container manifest.
-"""
+"""Creates a Container VM with the provided Container manifest."""
 
 
 COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
