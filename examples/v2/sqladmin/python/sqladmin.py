1d76dde9dd2d4a394a1ed2903e8b0e1299daa72f
salrashid123 <salrashid123@gmail.com>
2017-11-16 10:32:00 -0800

    remove add user

--- examples/v2/sqladmin/python/sqladmin.py
+++ examples/v2/sqladmin/python/sqladmin.py
@@ -55,28 +55,14 @@ def GenerateConfig(context):
           'instance': ''.join(['$(ref.', instance_name,'.name)']),
           'name': 'root',
           'host': '%'
-      }
-  }, {
-      'name': ''.join(['add-user-',context.properties['username']]),
-      'action': 'gcp-types/sqladmin-v1beta4:sql.users.insert',
-      'metadata': {
-          'runtimePolicy': ['CREATE'],
-          'dependsOn': [ 'delete-user-root', database_name ]
-      },
-      'properties': {
-          'project': context.env['project'],
-          'instance': ''.join(['$(ref.', instance_name,'.name)']),
-          'name': context.properties['username'],
-          'host': context.properties['host'],
-          'password': context.properties['password']
-      }
+      }     
   }]
 
   for n in range(0,context.properties['readReplicas']):
     resources.append({'name': ''.join([replica_name,'-',str(n)]),
                       'type': 'gcp-types/sqladmin-v1beta4:instances',
                       'metadata': {
-                         'dependsOn': [ database_name, ''.join(['add-user-',context.properties['username']]) ]
+                         'dependsOn': [ database_name ]
                       },                      
                       'properties': {
                           'region': context.properties['region'],
