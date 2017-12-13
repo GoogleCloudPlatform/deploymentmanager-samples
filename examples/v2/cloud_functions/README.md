652d7235834d4fe72df9149e868c0650dcdc0c56
Alejandro Jimenez Martinez <sondrago@google.com>
2017-12-13 13:29:11 -0800

    Make cloud function sample to deploy the code within the same deployment

--- examples/v2/cloud_functions/README.md
+++ examples/v2/cloud_functions/README.md
@@ -4,13 +4,14 @@
 
 This is a [Google Cloud Deployment
 Manager](https://cloud.google.com/deployment-manager/overview) template that
-deploys a Cloud Function, a PubSub topic, and calls the cloud function.
+deploys a Cloud Function, and calls the cloud function.
 
 ## Deploy the template
 
-Use `cloud_function.yaml` to deploy this example template. If you would like to
-use your own cloud function, you need to upload the content to your GCS bucket,
-and change the sourceArchiveUrl parameter
+Use `cloud_function.yaml` to deploy this example template.
+This template uses container builder to push the file contents into GCS so make
+sure you grant access to your Container Builder service account which is
+<projectNumber>@cloudbuild.gserviceaccount.com to be able to write to your bucket
 
 When ready, deploy with the following command:
 
@@ -18,14 +19,9 @@ When ready, deploy with the following command:
 
 ## Regarding the function source
 
-Our samples reference the function source (in the `function` directory) as
-`gs://cloud-function-sample/function.zip`. Cloud Functions doesn't have a
-way to inline the function code, so any function you write needs to be uploaded
-to a GCS bucket or use Cloud Source Repositories: instructions can be found
+Also it is possible to use Cloud Source Repositories: instructions can be found
 [here](https://cloud.google.com/functions/docs/deploying/filesystem) and
-[here](https://cloud.google.com/functions/docs/deploying/repo). Deployment
-Manager can't create zip files, so some these steps have to be done outside
-of your deployment.
+[here](https://cloud.google.com/functions/docs/deploying/repo).
 
 ## More information
 
--- examples/v2/cloud_functions/python/cloud_function.py
+++ examples/v2/cloud_functions/python/cloud_function.py
@@ -11,54 +11,106 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Creates a cloud function and calls it."""
 
+"""CloudFunction instance-type DM template.
+
+Provisions (or, creates) a pubsub topic.
+"""
+
+import base64
 import json
+import zipfile
+import hashlib
+from StringIO import StringIO
 
 
-def GenerateConfig(context):
+def GenerateConfig(ctx):
   """Generate YAML resource configuration."""
-  deployment_name = context.env['deployment']
-  function_call = deployment_name + '-function-call'
-  function_name = deployment_name + '-function'
-  topic_name = deployment_name + '-topic'
+  inMemoryOutputFile = StringIO()
+  function_name = ctx.env['deployment'] + 'cf'
+  function_call = ctx.env['deployment'] + '-function-call-' + str(ctx.env['current_time'])
+  source_archive_url = ctx.properties['sourceArchiveUrl']
+  zip_file = zipfile.ZipFile(inMemoryOutputFile, mode = 'w', compression = zipfile.ZIP_DEFLATED)
+  for imp in ctx.imports:
+    if imp.startswith('function/'):
+      zip_file.writestr(imp[len('function/'):], ctx.imports[imp])
+  zip_file.close()
+  content = base64.b64encode(inMemoryOutputFile.getvalue())
+  m = hashlib.md5()
+  m.update(content)
 
-  resources = [{
-      'name': topic_name,
-      'type': 'gcp-types/pubsub-v1:projects.topics',
-      'properties': {
-          'topic': topic_name,
-      }
-  }, {
-      'type': 'gcp-types/cloudfunctions-v1beta2:projects.locations.functions',
-      'name': function_name,
-      'properties': {
-          'location': context.properties['region'],
-          'function': function_name,
-          'sourceArchiveUrl': context.properties['sourceArchiveUrl'],
-          'entryPoint': context.properties['entryPoint'],
-          'eventTrigger': {
-              'resource': '$(ref.' + topic_name + '.name)',
-              'eventType': 'providers/cloud.pubsub/eventTypes/topic.publish'
-          }
-      }
-  }, {
-      'name':
-          function_call,
-      'action': ('gcp-types/cloudfunctions-v1beta2:'
-                 'cloudfunctions.projects.locations.functions.call'),
-      'properties': {
-          'name': '$(ref.' + function_name + '.name)',
-          'data': json.dumps({
-              'hola': 'mundo'
-          })
-      }
+  cmd = "echo '" + content + "' | base64 -d > /function/function.zip;"
+  volumes = [{
+    'name': 'function-code',
+    'path': '/function'
   }]
+  build_step = {
+    'name': 'upload-function-code',
+    'action': 'gcp-types/cloudbuild-v1:cloudbuild.projects.builds.create',
+    'metadata': {
+      'runtimePolicy': ['UPDATE_ON_CHANGE']
+    },
+    'properties': {
+      'steps': [{
+        'name': 'ubuntu',
+        'args': ['bash', '-c', cmd],
+        'volumes': volumes,
+      }, {
+        'name': 'gcr.io/cloud-builders/gsutil',
+        'args': ['cp', '/function/function.zip', source_archive_url],
+        'volumes': volumes
+      }],
+      'timeout': '120s'
+    }
+  }
+  cloud_function = {
+    'type': 'gcp-types/cloudfunctions-v1beta2:projects.locations.functions',
+    'name': function_name,
+    'properties': {
+      'location': ctx.properties['location'],
+      'function': function_name,
+      'labels': {
+        # Add the hash of the contents to trigger an update if the bucket
+        # object changes
+        'content-md5': m.hexdigest()
+      },
+      'sourceArchiveUrl': source_archive_url,
+      'entryPoint': ctx.properties['entryPoint'],
+      'httpsTrigger': {},
+      'timeout': ctx.properties['timeout'],
+      'availableMemoryMb': ctx.properties['availableMemoryMb']
+    },
+    'metadata': {
+      'dependsOn': ['upload-function-code']
+    }
+  }
+  resources = [build_step, cloud_function]
+  resources.append({
+    'name':
+      function_call,
+    'action': ('gcp-types/cloudfunctions-v1beta2:'
+               'cloudfunctions.projects.locations.functions.call'),
+    'metadata': {
+      'runtimePolicy': ['UPDATE_ALWAYS']
+    },
+    'properties': {
+      'name': '$(ref.' + function_name + '.name)',
+      'data': json.dumps({
+        'hola': 'mundo'
+      })
+    }
+  })
+
   return {
-      'resources':
-          resources,
-      'outputs': [{
-          'name': 'cloud-function-response',
-          'value': '$(ref.' + function_call + '.result)'
-      }]
+    'resources': resources,
+    'outputs': [
+      {
+        'name': 'sourceArchiveUrl',
+        'value': source_archive_url
+      },
+      {
+        'name': 'response',
+        'value': '$(ref.' + function_call + '.result)'
+      }
+    ]
   }
