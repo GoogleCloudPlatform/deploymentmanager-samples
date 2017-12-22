b3966fdc2d5cade9bdcf11a658a6edb9fbb7a2e7
yyxia <yyxia@google.com>
2017-12-21 16:10:23 -0800

    update project creation sample and set runtimePolicy of get-iam-policy action as UPDATE_ALWAYS

--- examples/v2/project_creation/project.py
+++ examples/v2/project_creation/project.py
@@ -89,6 +89,7 @@ def GenerateConfig(context):
   if context.properties.get('set-dm-service-account-as-owner'):
       # The name needs to be different in every update
       # due to a known issue in DM.
+      get_iam_policy_name = 'get-iam-policy'
       resources.extend([{
           'name': 'get-iam-policy-' + project_id,
           'action': 'gcp-types/cloudresourcemanager-v1:cloudresourcemanager.projects.getIamPolicy',
