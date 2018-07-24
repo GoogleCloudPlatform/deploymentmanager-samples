e6af2b81673107270327400fbfa5e72f2dd202be
Adam Ocsvari <ocsi@google.com>
2018-07-23 23:20:50 -0700

    updating master config
    fixing typo in config property.

--- community/hierarchical_configuration/configs/master_config.py
+++ community/hierarchical_configuration/configs/master_config.py
@@ -1,6 +1,6 @@
 config ={
 	'CONTEXT' : 'We are in Master context',
-	'Org_Name': 'Sample Inc',
+	'Org_Name': 'Sample-Inc',
 	'Department_name': 'Department of silly walks',
 	'HQ_Address': { 'City': 'London', 'Country': 'UK'}
 }
