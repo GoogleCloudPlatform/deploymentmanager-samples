12ef850f83816fcebc2934787c34de1563c259d8
Alexander Levitski <alexander.levitski@sourcedgroup.com>
2018-08-29 15:29:02 -0400

    Cloud Router - documemntation edited

--- community/cloud-foundation/templates/cloud_router/cloud_router.py
+++ community/cloud-foundation/templates/cloud_router/cloud_router.py
@@ -11,11 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Creates a Cloud Router."""
+
+""" This template creates a Cloud Router. """
 
 
 def generate_config(context):
-    """ Entry point for the deployment resources """
+    """ Entry point for the deployment resources. """
 
     resources = [
         {
@@ -53,7 +54,7 @@ def generate_config(context):
 
 
 def generate_network_url(context, network):
-    """Format the resource name to a resource URI"""
+    """Format the resource name as a resource URI."""
 
     return 'projects/{}/global/networks/{}'.format(
         context.env['project'],
