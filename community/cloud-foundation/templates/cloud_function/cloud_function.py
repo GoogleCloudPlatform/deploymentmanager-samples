fe9835c0ca8ca9e6bf631936ae563e5b369b9066
Fatima <fawix@users.noreply.github.com>
2018-09-30 15:20:52 -0400

    updated header comment

--- community/cloud-foundation/templates/cloud_function/cloud_function.py
+++ community/cloud-foundation/templates/cloud_function/cloud_function.py
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" This template creates a Cloud Function. """
+""" Creates a Cloud Function from a local file system, a Cloud Storage bucket, 
+    or a Cloud Source Repository, and then assigns an HTTPS, Storage, or Pub/Sub 
+    trigger to that Cloud Function. 
+"""
 
 NO_RESOURCES_OR_OUTPUTS = [], []
 
