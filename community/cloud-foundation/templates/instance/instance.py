f31b2c38a9d074826d78c7bbf808db5fd1834ff7
Fatima <fawix@users.noreply.github.com>
2018-09-30 17:47:23 -0400

    minor change on comment

--- community/cloud-foundation/templates/instance/instance.py
+++ community/cloud-foundation/templates/instance/instance.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" This template creates a Compute instance."""
+""" This template creates a Compute Instance."""
 
 def set_optional_property(receiver, source, property_name):
     """ If set, copies the given property value from one object to another. """
