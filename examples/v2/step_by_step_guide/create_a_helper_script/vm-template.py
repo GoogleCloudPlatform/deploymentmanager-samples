c3f0c848615457f15c7c40518f76289f07ace8c4
Anita Garg <anitagarg@google.com>
2019-08-16 14:13:07 -0700

    Add common, two-vms, and vm-template to this directory

---
+++ examples/v2/step_by_step_guide/create_a_helper_script/vm-template.py
@@ -0,0 +1,42 @@
+"""Creates the virtual machine."""
+
+{% import 'common.py' as common %}
+
+COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
+
+
+def GenerateConfig(context):
+  """Creates the virtual machine with environment variables."""
+
+  resources = [{
+      'name': {{ common.GenerateMachineName("myfrontend", "prod") }},
+      'type': 'compute.v1.instance',
+      'properties': {
+          'zone': context.properties['zone'],
+          'machineType': ''.join([COMPUTE_URL_BASE, 'projects/',
+                                  context.env['project'], '/zones/',
+                                  context.properties['zone'], '/machineTypes/',
+                                  context.properties['machineType']]),
+          'disks': [{
+              'deviceName': 'boot',
+              'type': 'PERSISTENT',
+              'boot': True,
+              'autoDelete': True,
+              'initializeParams': {
+                  'sourceImage': ''.join([COMPUTE_URL_BASE, 'projects/',
+                                          'debian-cloud/global/',
+                                          'images/family/debian-9'])
+              }
+          }],
+          'networkInterfaces': [{
+              'network': '$(ref.' + context.properties['network']
+                         + '.selfLink)',
+              'accessConfigs': [{
+                  'name': 'External NAT',
+                  'type': 'ONE_TO_ONE_NAT'
+              }]
+          }]
+      }
+  }]
+  return {'resources': resources}
+
