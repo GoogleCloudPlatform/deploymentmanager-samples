09fbca15fe25d11f1916df7e57008a0c52416054
Anita Garg <anitagarg@google.com>
2019-08-21 15:18:06 -0700

    update vm-template.py to correctly import helper script

--- examples/v2/step_by_step_guide/create_a_helper_script/vm-template.py
+++ examples/v2/step_by_step_guide/create_a_helper_script/vm-template.py
@@ -1,14 +1,13 @@
 """Creates the virtual machine."""
 
 # `common.py` is imported below.
-{% import 'common.py' as common %}
+from helpers import common
 
 COMPUTE_URL_BASE = 'https://www.googleapis.com/compute/v1/'
 
 
 def GenerateConfig(context):
   """Creates the virtual machine with environment variables."""
-  
   # The `name` listing has been changed to use the helper script.
   resources = [{
       'name': {{ common.GenerateMachineName("myfrontend", "prod") }},
