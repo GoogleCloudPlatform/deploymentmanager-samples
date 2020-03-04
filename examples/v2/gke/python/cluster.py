b4113761adf8a94b0f5f2c112e9cffc6789a3e64
Inger Klekacz <inger.klekacz@leanpath.org>
2020-03-04 15:40:47 -0800

    making indentations PEP8-compliant per https://www.python.org/dev/peps/pep-0008/#indentation

--- examples/v2/gke/python/cluster.py
+++ examples/v2/gke/python/cluster.py
@@ -17,91 +17,91 @@ import six
 
 
 def GenerateConfig(context):
-  """Generate YAML resource configuration."""
+    """Generate YAML resource configuration."""
 
-  name_prefix = context.env['deployment'] + '-' + context.env['name']
-  cluster_name = name_prefix
-  type_name = name_prefix + '-type'
-  k8s_endpoints = {
-      '': 'api/v1',
-      '-apps': 'apis/apps/v1beta1',
-      '-v1beta1-extensions': 'apis/extensions/v1beta1'
-  }
+    name_prefix = context.env['deployment'] + '-' + context.env['name']
+    cluster_name = name_prefix
+    type_name = name_prefix + '-type'
+    k8s_endpoints = {
+        '': 'api/v1',
+        '-apps': 'apis/apps/v1beta1',
+        '-v1beta1-extensions': 'apis/extensions/v1beta1'
+    }
 
-  resources = [
-      {
-          'name': cluster_name,
-          'type': 'container.v1.cluster',
-          'properties': {
-              'zone': context.properties['zone'],
-              'cluster': {
-                  'name': cluster_name,
-                  'initialNodeCount': context.properties['initialNodeCount'],
-                  'nodeConfig': {
-                      'oauthScopes': [
-                          'https://www.googleapis.com/auth/' + s
-                          for s in [
-                              'compute',
-                              'devstorage.read_only',
-                              'logging.write',
-                              'monitoring'
-                          ]
-                      ]
-                  }
-              }
-          }
-      }
-  ]
-  outputs = []
-  for type_suffix, endpoint in six.iteritems(k8s_endpoints):
-    resources.append({
-        'name': type_name + type_suffix,
-        'type': 'deploymentmanager.v2beta.typeProvider',
-        'properties': {
-            'options': {
-                'validationOptions': {
-                    # Kubernetes API accepts ints, in fields they annotate
-                    # with string. This validation will show as warning
-                    # rather than failure for Deployment Manager.
-                    # https://github.com/kubernetes/kubernetes/issues/2971
-                    'schemaValidation': 'IGNORE_WITH_WARNINGS'
-                },
-                # According to kubernetes spec, the path parameter 'name'
-                # should be the value inside the metadata field
-                # https://github.com/kubernetes/community/blob/master
-                # /contributors/devel/api-conventions.md
-                # This mapping specifies that
-                'inputMappings': [{
-                    'fieldName': 'name',
-                    'location': 'PATH',
-                    'methodMatch': '^(GET|DELETE|PUT)$',
-                    'value': '$.ifNull('
-                             '$.resource.properties.metadata.name, '
-                             '$.resource.name)'
-                }, {
-                    'fieldName': 'metadata.name',
-                    'location': 'BODY',
-                    'methodMatch': '^(PUT|POST)$',
-                    'value': '$.ifNull('
-                             '$.resource.properties.metadata.name, '
-                             '$.resource.name)'
-                }, {
-                    'fieldName': 'Authorization',
-                    'location': 'HEADER',
-                    'value': '$.concat("Bearer ",'
-                             '$.googleOauth2AccessToken())'
-                }]
-            },
-            'descriptorUrl':
-                ''.join([
-                    'https://$(ref.', cluster_name, '.endpoint)/swaggerapi/',
-                    endpoint
-                ])
+    resources = [
+        {
+            'name': cluster_name,
+            'type': 'container.v1.cluster',
+            'properties': {
+                'zone': context.properties['zone'],
+                'cluster': {
+                    'name': cluster_name,
+                    'initialNodeCount': context.properties['initialNodeCount'],
+                    'nodeConfig': {
+                        'oauthScopes': [
+                            'https://www.googleapis.com/auth/' + s
+                            for s in [
+                                'compute',
+                                'devstorage.read_only',
+                                'logging.write',
+                                'monitoring'
+                            ]
+                        ]
+                    }
+                }
+            }
         }
-    })
-    outputs.append({
-        'name': 'clusterType' + type_suffix,
-        'value': type_name + type_suffix
-    })
+    ]
+    outputs = []
+    for type_suffix, endpoint in six.iteritems(k8s_endpoints):
+        resources.append({
+            'name': type_name + type_suffix,
+            'type': 'deploymentmanager.v2beta.typeProvider',
+            'properties': {
+                'options': {
+                    'validationOptions': {
+                        # Kubernetes API accepts ints, in fields they annotate
+                        # with string. This validation will show as warning
+                        # rather than failure for Deployment Manager.
+                        # https://github.com/kubernetes/kubernetes/issues/2971
+                        'schemaValidation': 'IGNORE_WITH_WARNINGS'
+                    },
+                    # According to kubernetes spec, the path parameter 'name'
+                    # should be the value inside the metadata field
+                    # https://github.com/kubernetes/community/blob/master
+                    # /contributors/devel/api-conventions.md
+                    # This mapping specifies that
+                    'inputMappings': [{
+                        'fieldName': 'name',
+                        'location': 'PATH',
+                        'methodMatch': '^(GET|DELETE|PUT)$',
+                        'value': '$.ifNull('
+                                    '$.resource.properties.metadata.name, '
+                                    '$.resource.name)'
+                    }, {
+                        'fieldName': 'metadata.name',
+                        'location': 'BODY',
+                        'methodMatch': '^(PUT|POST)$',
+                        'value': '$.ifNull('
+                                    '$.resource.properties.metadata.name, '
+                                    '$.resource.name)'
+                    }, {
+                        'fieldName': 'Authorization',
+                        'location': 'HEADER',
+                        'value': '$.concat("Bearer ",'
+                                    '$.googleOauth2AccessToken())'
+                    }]
+                },
+                'descriptorUrl':
+                    ''.join([
+                        'https://$(ref.', cluster_name, '.endpoint)/swaggerapi/',
+                        endpoint
+                    ])
+            }
+        })
+        outputs.append({
+            'name': 'clusterType' + type_suffix,
+            'value': type_name + type_suffix
+        })
 
-  return {'resources': resources, 'outputs': outputs}
+    return {'resources': resources, 'outputs': outputs}
--- examples/v2/gke/python/deployment.py
+++ examples/v2/gke/python/deployment.py
@@ -15,86 +15,86 @@
 
 
 def GenerateConfig(context):
-  """Generate YAML resource configuration."""
+    """Generate YAML resource configuration."""
 
-  cluster_types_root = ''.join([
-      context.env['project'],
-      '/',
-      context.properties['clusterType']
-      ])
-  cluster_types = {
-      'Service': ''.join([
-          cluster_types_root,
-          ':',
-          '/api/v1/namespaces/{namespace}/services'
-          ]),
-      'Deployment': ''.join([
-          cluster_types_root,
-          '-apps',
-          ':',
-          '/apis/apps/v1beta1/namespaces/{namespace}/deployments'
-          ])
-  }
+    cluster_types_root = ''.join([
+        context.env['project'],
+        '/',
+        context.properties['clusterType']
+        ])
+    cluster_types = {
+        'Service': ''.join([
+            cluster_types_root,
+            ':',
+            '/api/v1/namespaces/{namespace}/services'
+            ]),
+        'Deployment': ''.join([
+            cluster_types_root,
+            '-apps',
+            ':',
+            '/apis/apps/v1beta1/namespaces/{namespace}/deployments'
+            ])
+    }
 
-  name_prefix = context.env['deployment'] + '-' + context.env['name']
-  port = context.properties['port']
+    name_prefix = context.env['deployment'] + '-' + context.env['name']
+    port = context.properties['port']
 
-  resources = [{
-      'name': name_prefix + '-service',
-      'type': cluster_types['Service'],
-      'properties': {
-          'apiVersion': 'v1',
-          'kind': 'Service',
-          'namespace': 'default',
-          'metadata': {
-              'name': name_prefix + '-service',
-              'labels': {
-                  'id': 'deployment-manager'
-              }
-          },
-          'spec': {
-              'type': 'NodePort',
-              'ports': [{
-                  'port': port,
-                  'targetPort': port,
-                  'protocol': 'TCP'
-              }],
-              'selector': {
-                  'app': name_prefix
-              }
-          }
-      }
-  }, {
-      'name': name_prefix + '-deployment',
-      'type': cluster_types['Deployment'],
-      'properties': {
-          'apiVersion': 'apps/v1beta1',
-          'kind': 'Deployment',
-          'namespace': 'default',
-          'metadata': {
-              'name': name_prefix + '-deployment'
-          },
-          'spec': {
-              'replicas': 1,
-              'template': {
-                  'metadata': {
-                      'labels': {
-                          'name': name_prefix + '-deployment',
-                          'app': name_prefix
-                      }
-                  },
-                  'spec': {
-                      'containers': [{
-                          'name': 'container',
-                          'image': context.properties['image'],
-                          'ports': [{
-                              'containerPort': port
-                          }]
-                      }]
-                  }
-              }
-          }
-      }
-  }]
+    resources = [{
+        'name': name_prefix + '-service',
+        'type': cluster_types['Service'],
+        'properties': {
+            'apiVersion': 'v1',
+            'kind': 'Service',
+            'namespace': 'default',
+            'metadata': {
+                'name': name_prefix + '-service',
+                'labels': {
+                    'id': 'deployment-manager'
+                }
+            },
+            'spec': {
+                'type': 'NodePort',
+                'ports': [{
+                    'port': port,
+                    'targetPort': port,
+                    'protocol': 'TCP'
+                }],
+                'selector': {
+                    'app': name_prefix
+                }
+            }
+        }
+    }, {
+        'name': name_prefix + '-deployment',
+        'type': cluster_types['Deployment'],
+        'properties': {
+            'apiVersion': 'apps/v1beta1',
+            'kind': 'Deployment',
+            'namespace': 'default',
+            'metadata': {
+                'name': name_prefix + '-deployment'
+            },
+            'spec': {
+                'replicas': 1,
+                'template': {
+                    'metadata': {
+                        'labels': {
+                            'name': name_prefix + '-deployment',
+                            'app': name_prefix
+                        }
+                    },
+                    'spec': {
+                        'containers': [{
+                            'name': 'container',
+                            'image': context.properties['image'],
+                            'ports': [{
+                                'containerPort': port
+                            }]
+                        }]
+                    }
+                }
+            }
+        }
+    }]
 
-  return {'resources': resources}
+    return {'resources': resources}
