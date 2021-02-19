ca40b70c0c01880c104fb447b88b7d709fe7b994
Namrata S <16926363+NamrataShah5@users.noreply.github.com>
2021-02-19 11:23:18 -0800

    Update test_alternatives.sh
    Working on TF and DM

--- google/resource-snippets/pubsub-v1/test_alternatives.sh
+++ google/resource-snippets/pubsub-v1/test_alternatives.sh
@@ -19,11 +19,6 @@ terraform init
 terraform apply -auto-approve -var="deployment=tf" -var="project_id=${TF_PROJECT_ID}"
 # popd
 
-#Create KCC resources
-gcloud config set project $KRM_PROJECT_ID
-gcloud container clusters get-credentials {CLUSTER_NAME} --zone us-central1-c
-kubectl apply --namespace krm -f pubsub.yaml 
-
 
 # Export DM and TF resources for comparison
 gcloud pubsub subscriptions list --filter="labels.goog-dm:dm" --project $DM_PROJECT_ID | sed "s/${DM_PROJECT_ID}/PROJECT/" > dm.yaml
@@ -31,8 +26,6 @@ gcloud pubsub subscriptions list --filter="labels.goog-dm:dm" --project $DM_PROJ
 gcloud pubsub subscriptions list --filter=topic:my-pubsub-topic --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  > tf.yaml
 gcloud pubsub subscriptions list --filter=topic:my-backup-topic --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  >> tf.yaml
 
-# gcloud pubsub subscriptions list --filter=topic:my-pubsub-topic --project {KRM_PROJECT_ID} --format=yaml > krm.yaml
-# gcloud pubsub subscriptions list --filter=topic:my-backup-topic --project {KRM_PROJECT_ID} --format=yaml >> krm.yaml
 
 diff tf.yaml dm.yaml
 
@@ -43,13 +36,5 @@ else
     echo "TF and DM outputs are identical"
 fi
 
-#diff /tmp/${KRM_PROJECT_ID}.yaml /tmp/${DM_PROJECT_ID}.yaml
-
-#if [[ $(diff /tmp/${KRM_PROJECT_ID}.yaml /tmp/${DM_PROJECT_ID}.yaml) ]]; then
-#    echo "KRM and DM outputs are NOT identical"
-#    exit 1
-#else
-#    echo "KRM and DM outputs are identical"
-#fi
 
 exit 0
