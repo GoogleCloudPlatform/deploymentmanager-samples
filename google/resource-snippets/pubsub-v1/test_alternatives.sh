9f4d1ffb6c3fbb6ddf577df321ade1f8f7008c83
Namrata S <16926363+NamrataShah5@users.noreply.github.com>
2021-02-19 17:37:48 -0800

    Update test_alternatives.sh

--- google/resource-snippets/pubsub-v1/test_alternatives.sh
+++ google/resource-snippets/pubsub-v1/test_alternatives.sh
@@ -1,7 +1,6 @@
 set -e
 
 TF_PROJECT_ID={TF_PROJECT_ID}
-KRM_PROJECT_ID={KRM_PROJECT_ID}
 DM_PROJECT_ID={DM_PROJECT_ID}
 
 
@@ -16,25 +15,21 @@ gcloud deployment-manager deployments create d1 --config pubsub.yaml
 gcloud config set project $TF_PROJECT_ID
 gcloud config list --format 'value(core.project)'
 terraform init
+terraform plan -var="deployment=d1" -var="project_id=${TF_PROJECT_ID}"
 terraform apply -auto-approve -var="deployment=d1" -var="project_id=${TF_PROJECT_ID}"
 
 
 # Export DM and TF resources for comparison
 gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $DM_PROJECT_ID | sed "s/${DM_PROJECT_ID}/PROJECT/" > dm.yaml
 
-gcloud pubsub subscriptions list --filter=topic:my-pubsub-topic --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  > tf.yaml
-gcloud pubsub subscriptions list --filter=topic:my-backup-topic --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  >> tf.yaml
+gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  > tf.yaml
 
 
-sort tf.yaml > tf.yaml
-sort dm.yaml > dm.yaml
-
-if [[ $(diff dm.yaml tf.yaml) ]]; then
+if [[ $(diff tf.yaml dm.yaml) ]]; then
     echo "TF and DM outputs are NOT identical"
     exit 1
 else
     echo "TF and DM outputs are identical"
 fi
-
 
 exit 0
