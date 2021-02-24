eac427d004844aaf3e72ef089ac1f0437cd22dd7
Namrata S <16926363+NamrataShah5@users.noreply.github.com>
2021-02-24 10:11:40 -0800

    Update pubsub.tf

--- google/resource-snippets/pubsub-v1/alternatives/tf/pubsub.tf
+++ google/resource-snippets/pubsub-v1/alternatives/tf/pubsub.tf
@@ -25,12 +25,6 @@ resource "google_pubsub_topic" "my-topic" {
   }
 }
 
-resource "google_pubsub_topic" "my-backup-topic" {
-  name="{BACKUP_TOPIC_NAME}"
-  labels= {
-    goog-dm=var.deployment
-  }
-}
 
 resource "google_pubsub_subscription" "my-subscription" {
   name = "{SUBSCRIPTION_NAME}"
@@ -40,29 +34,6 @@ resource "google_pubsub_subscription" "my-subscription" {
   topic = google_pubsub_topic.my-topic.name
   message_retention_duration="{MESSAGE_RETENTION_DURATION}"
   retain_acked_messages={TRUE,FALSE}
-  ack_deadline_seconds = {ACK_DEADLINE_SECONDS}
-  dead_letter_policy {
-    dead_letter_topic = google_pubsub_topic.my-backup-topic.id
-    max_delivery_attempts={MAX_DELIVERY_ATTEMPTS}
-
-  }
-  expiration_policy {
-    ttl="{TTL}"
-  }
-  retry_policy {
-    minimum_backoff="{MINIMUM_BACKOFF}"
-    maximum_backoff= "{MAXIMUM_BACKOFF}"
-
-  }
-  
-  resource "google_pubsub_subscription" "my-deadletter-subscription" {
-    name = "{DEADLETTER_SUBSCRIPTION_NAME}"
-    labels= {
-      goog-dm=var.deployment
-    }
-    topic = google_pubsub_topic.my-deadletter-subscription.name
-
-  }
-
 }
+
 
