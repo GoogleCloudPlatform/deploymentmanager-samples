eaed2409a4bae186dccd1ae41afd72c4ab75e8a4
Namrata S <16926363+NamrataShah5@users.noreply.github.com>
2021-02-23 16:45:07 -0800

    Update test_alternatives.sh

--- google/resource-snippets/pubsub-v1/test_alternatives.sh
+++ google/resource-snippets/pubsub-v1/test_alternatives.sh
@@ -12,19 +12,21 @@ gcloud services enable deploymentmanager.googleapis.com
 gcloud deployment-manager deployments create d1 --config pubsub.yaml
 
 # Create Terraform resources
+pushd alternatives/tf
 gcloud config set project $TF_PROJECT_ID
 terraform init
 terraform plan -var="deployment=d1" -var="project_id=${TF_PROJECT_ID}"
 terraform apply -auto-approve -var="deployment=d1" -var="project_id=${TF_PROJECT_ID}"
+popd
 
 
 # Export DM and TF resources for comparison
-gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $DM_PROJECT_ID | sed "s/${DM_PROJECT_ID}/PROJECT/" > dm.yaml
+gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $DM_PROJECT_ID | sed "s/${DM_PROJECT_ID}/PROJECT/" > /tmp/dm.yaml
 
-gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  > tf.yaml
+gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  > /tmp/tf.yaml
 
 
-if [[ $(diff tf.yaml dm.yaml) ]]; then
+if [[ $(diff /tmp/dm.yaml /tmp/tf.yaml) ]]; then
     echo "TF and DM outputs are NOT identical"
     exit 1
 else
