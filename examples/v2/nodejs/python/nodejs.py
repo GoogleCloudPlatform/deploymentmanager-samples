8b013e2ea2ca36a0436f4b663502f0476cc8066b
akshaykumarpatil-tudip <akshaykumar.patil@tudip.com>
2021-03-03 22:41:22 +0530

    Update: Deployment Manager - Full Production [ACE] - Updated the below changes in nodejs.py file as necessary. - Changed the port back to MySQL default i.e. 3306 - Added MySQL Docker environment vars for the MYSQL_ROOT_PASSWORD - Updated gcr.io/qwiklabs-resources/mysql - Updated gcr.io/qwiklabs-resources/nodejsservice

--- examples/v2/nodejs/python/nodejs.py
+++ examples/v2/nodejs/python/nodejs.py
@@ -22,22 +22,25 @@ def GenerateConfig(context):
   frontend = context.env['deployment'] + '-frontend'
   firewall = context.env['deployment'] + '-application-fw'
   application_port = 8080
-  mysql_port = 8080
+  mysql_port = 3306
   resources = [{
       'name': backend,
       'type': 'container_vm.py',
       'properties': {
           'zone': context.properties['zone'],
-          'dockerImage': 'gcr.io/deployment-manager-examples/mysql',
+          'dockerImage': 'gcr.io/qwiklabs-resources/mysql',
           'containerImage': 'family/cos-stable',
-          'port': mysql_port
+          'port': mysql_port,
+          'dockerEnv': {
+              'MYSQL_ROOT_PASSWORD': 'mypassword'
+          }
       }
   }, {
       'name': frontend,
       'type': 'frontend.py',
       'properties': {
           'zone': context.properties['zone'],
-          'dockerImage': 'gcr.io/deployment-manager-examples/nodejsservice',
+          'dockerImage': 'gcr.io/qwiklabs-resources/nodejsservice',
           'port': application_port,
           # Define the variables that are exposed to container as env variables.
           'dockerEnv': {
@@ -48,7 +51,7 @@ def GenerateConfig(context):
           # If left out will default to 1
           'size': 2,
           # If left out will default to 1
-          'maxSize': 20
+          'maxSize': 4
       }
   }, {
       'name': firewall,
