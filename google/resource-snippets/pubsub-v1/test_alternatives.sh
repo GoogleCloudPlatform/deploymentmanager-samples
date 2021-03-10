3346e6dda7722e4e3739f97fdc7e0277c9bf2f2d
Namrata S <16926363+NamrataShah5@users.noreply.github.com>
2021-03-10 10:38:50 -0800

    Add pubsub KRM files (#625)
    * Create pubsub.yaml
    
    * Create Kptfile
    
    * Update pubsub.yaml
    
    * Update Kptfile
    
    * Update Kptfile
    
    * Update test_alternatives.sh
    
    * Update test_alternatives.sh
    
    * Update Kptfile
    
    * Update pubsub.yaml
    
    * Update test_alternatives.sh
    
    * Update Kptfile
    
    * Update Kptfile
    
    * Update pubsub.yaml
    
    * Update test_alternatives.sh
    
    * Update README.md
    
    * Update test_alternatives.sh
    
    * Update pubsub.yaml
    
    * Update Kptfile
    
    * Update pubsub.yaml
    
    * Update test_alternatives.sh
    
    * Update test_alternatives.sh
    
    * Update pubsub.yaml
    
    * Update test_alternatives.sh

--- google/resource-snippets/pubsub-v1/test_alternatives.sh
+++ google/resource-snippets/pubsub-v1/test_alternatives.sh
@@ -2,7 +2,7 @@ set -e
 
 TF_PROJECT_ID={TF_PROJECT_ID}
 DM_PROJECT_ID={DM_PROJECT_ID}
-
+KRM_PROJECT_ID={KRM_PROJECT_ID}
 
 # Create DM resources
 gcloud config set project $DM_PROJECT_ID
@@ -28,11 +28,23 @@ terraform plan -var="deployment=d1" -var="project_id=${TF_PROJECT_ID}"
 terraform apply -auto-approve -var="deployment=d1" -var="project_id=${TF_PROJECT_ID}"
 popd
 
+# KCC Configuration
+gcloud config set project $KRM_PROJECT_ID
+cp -R alternatives/krm /tmp/krm_${KRM_PROJECT_ID}
+pushd /tmp/krm_${KRM_PROJECT_ID}
+kpt cfg set . PUBSUB my-pubsub
+kubectl apply -f pubsub.yaml
+kubectl  wait --for=condition=Ready PubSubSubscription --all
+popd
+rm -rf krm_${KRM_PROJECT_ID}
+
 
 # Export DM and TF resources for comparison
 gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $DM_PROJECT_ID | sed "s/${DM_PROJECT_ID}/PROJECT/" > /tmp/dm.yaml
 
 gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $TF_PROJECT_ID | sed "s/${TF_PROJECT_ID}/PROJECT/"  > /tmp/tf.yaml
+
+gcloud pubsub subscriptions list --filter="labels.goog-dm:d1" --project $KRM_PROJECT_ID | sed "s/${KRM_PROJECT_ID}/PROJECT/" | sed "s/creationTime: .*/creationTime: TIME/" | sed "/cnrm-lease-.*/d" | sed "/managed-by-cnrm.*/d"  > /tmp/krm.yaml
 
 
 if [[ $(diff /tmp/dm.yaml /tmp/tf.yaml) ]]; then
@@ -40,6 +52,13 @@ if [[ $(diff /tmp/dm.yaml /tmp/tf.yaml) ]]; then
     exit 1
 else
     echo "TF and DM outputs are identical"
+fi
+
+if [[ $(diff /tmp/dm.yaml /tmp/krm.yaml) ]]; then
+    echo "KRM and DM outputs are NOT identical"
+    exit 1
+else
+    echo "KRM and DM outputs are identical"
 fi
 
 exit 0
