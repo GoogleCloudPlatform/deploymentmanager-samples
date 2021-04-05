9755e3f789ffb5df1dd6ccc73fd5b1b3e8839cc3
Yanshu Zhao <zyanshu@google.com>
2021-04-05 02:09:59 +0000

    Refactor test script

--- google/resource-snippets/logging-v2/test_alternatives.sh
+++ google/resource-snippets/logging-v2/test_alternatives.sh
@@ -1,39 +1,57 @@
 set -e
 
-# assumes that the following projects are created and kubectl context is set to a cluster in KRM_PROJECT
-TF_PROJECT_ID=[TF_PROJECT_ID]
-DM_PROJECT_ID=[DM_PROJECT_ID]
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+RESET_COLOR='\033[0m'
 
-gcloud auth application-default login
+provision_using_dm() {
+  gcloud deployment-manager deployments create deployment --config logging.yaml
+  gcloud logging sinks list --location us-central1 --filter="labels.goog-dm:deployment" --project $PROJECT_ID > /tmp/dm.yaml  
+  gcloud deployment-manager deployments delete deployment -q
+}
 
-# Create DM resources
-gcloud config set project $DM_PROJECT_ID
-gcloud services enable deploymentmanager.googleapis.com --project $DM_PROJECT_ID
-gcloud services enable cloudkms.googleapis.com --project $DM_PROJECT_ID
-gcloud deployment-manager deployments create d1 --config logging.yaml
+provision_using_tf() {
+  if [[ -z "${GOOGLE_CREDENTIALS}" ]]; then
+    # For pub/sub, the ADC will have sufficient permissions
+    echo "Fetching Application Default Credentials for Terraform"
+    gcloud auth application-default login
+    export GOOGLE_CREDENTIALS=~/.config/gcloud/application_default_credentials.json
+  fi
 
-# Create Terraform resources
-pushd alternatives/tf
-gcloud config set project $TF_PROJECT_ID
-gcloud services enable cloudkms.googleapis.com --project $TF_PROJECT_ID
-terraform plan -var="deployment=d1" -var="filter=severity >= ERROR" -var="project_id=${TF_PROJECT_ID}"
-terraform apply -auto-approve -var="deployment=d1" -var="filter=severity >= ERROR" -var="project_id=${TF_PROJECT_ID}"
-popd
+  cp -R alternatives/tf/ /tmp/tf_"${PROJECT_ID}"
+  pushd /tmp/tf_"${PROJECT_ID}"
+  terraform init
+  terraform plan -var="deployment=deployment" -var="filter=severity >= ERROR" -var="project_id=${PROJECT_ID}"
+  terraform apply -auto-approve -var="deployment=deployment" -var="filter=severity >= ERROR" -var="project_id=${PROJECT_ID}"
+  gcloud logging sinks list --location us-central1 --filter="labels.goog-tf:deployment" --project $PROJECT_ID > /tmp/tf.yaml  
+  terraform destroy -auto-approve -var="deployment=deployment" -var="project_id=${PROJECT_ID}"
+  popd
+  rm -rf /tmp/tf_${PROJECT_ID}
+}
 
-# Export DM and TF resources for comparison
-gcloud logging sinks list --location us-central1 --project $DM_PROJECT_ID | grep d1 | sed "s/${DM_PROJECT_ID}/PROJECT/" \
-> /tmp/${DM_PROJECT_ID}.yaml
+gcloud config set project "${PROJECT_ID}"
 
-gcloud logging sinks list --location us-central1 --project $TF_PROJECT_ID | grep d1 | sed "s/${TF_PROJECT_ID}/PROJECT/" \
-> /tmp/${TF_PROJECT_ID}.yaml
-
-diff /tmp/${TF_PROJECT_ID}.yaml /tmp/${DM_PROJECT_ID}.yaml
-
-if [[ $(diff /tmp/${TF_PROJECT_ID}.yaml /tmp/${DM_PROJECT_ID}.yaml) ]]; then
-    echo "TF and DM outputs are NOT identical"
-    exit 1
+if [[ -n $( gcloud auth list --filter=status:ACTIVE --format="value(account)" ) ]]; then
+  account_name=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
+  echo "Reusing $account_name user credentials"
 else
-    echo "TF and DM outputs are identical"
+  gcloud auth login
 fi
 
+gcloud services enable deploymentmanager.googleapis.com
+gcloud services enable logging.googleapis.com
+
+provision_using_dm
+provision_using_tf
+
+if [[ -n $(diff /tmp/dm.yaml /tmp/tf.yaml) ]]; then
+    echo "${RED_COLOR}TF and DM outputs are NOT identical${RESET_COLOR}"
+    echo "diff /tmp/dm.yaml /tmp/tf.yaml"
+    diff /tmp/dm.yaml /tmp/tf.yaml
+    exit 1
+else
+    echo "${GREEN_COLOR}TF and DM outputs are identical${RESET_COLOR}"
+fi
+
+echo "${GREEN_COLOR}Test Success${RESET_COLOR}"
 exit 0
