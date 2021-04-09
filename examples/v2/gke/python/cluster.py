a35144ff205c25f14eb0dd4fb6e64a4cbf91f1a2
Kevin Emery <kevinemery@google.com>
2021-04-08 17:05:46 -0700

    Update GKE templates from swaggerapi to openapi
    These examples have been out of date for a bit since swaggerapi became
    deprecated within GKE. This updates them to openapi and also moves
    the DM type to the newer, more update type definition
    
    Fixes #618
    Fixes #619

--- examples/v2/gke/python/cluster.py
+++ examples/v2/gke/python/cluster.py
@@ -22,16 +22,11 @@ def GenerateConfig(context):
   name_prefix = context.env['deployment'] + '-' + context.env['name']
   cluster_name = name_prefix
   type_name = name_prefix + '-type'
-  k8s_endpoints = {
-      '': 'api/v1',
-      '-apps': 'apis/apps/v1beta1',
-      '-v1beta1-extensions': 'apis/extensions/v1beta1'
-  }
 
   resources = [
       {
           'name': cluster_name,
-          'type': 'container.v1.cluster',
+          'type': 'gcp-types/container-v1:projects.zones.clusters',
           'properties': {
               'zone': context.properties['zone'],
               'cluster': {
@@ -52,56 +47,64 @@ def GenerateConfig(context):
           }
       }
   ]
-  outputs = []
-  for type_suffix, endpoint in six.iteritems(k8s_endpoints):
-    resources.append({
-        'name': type_name + type_suffix,
-        'type': 'deploymentmanager.v2beta.typeProvider',
-        'properties': {
-            'options': {
-                'validationOptions': {
-                    # Kubernetes API accepts ints, in fields they annotate
-                    # with string. This validation will show as warning
-                    # rather than failure for Deployment Manager.
-                    # https://github.com/kubernetes/kubernetes/issues/2971
-                    'schemaValidation': 'IGNORE_WITH_WARNINGS'
-                },
-                # According to kubernetes spec, the path parameter 'name'
-                # should be the value inside the metadata field
-                # https://github.com/kubernetes/community/blob/master
-                # /contributors/devel/api-conventions.md
-                # This mapping specifies that
-                'inputMappings': [{
-                    'fieldName': 'name',
-                    'location': 'PATH',
-                    'methodMatch': '^(GET|DELETE|PUT)$',
-                    'value': '$.ifNull('
-                             '$.resource.properties.metadata.name, '
-                             '$.resource.name)'
-                }, {
-                    'fieldName': 'metadata.name',
-                    'location': 'BODY',
-                    'methodMatch': '^(PUT|POST)$',
-                    'value': '$.ifNull('
-                             '$.resource.properties.metadata.name, '
-                             '$.resource.name)'
-                }, {
-                    'fieldName': 'Authorization',
-                    'location': 'HEADER',
-                    'value': '$.concat("Bearer ",'
-                             '$.googleOauth2AccessToken())'
-                }]
-            },
-            'descriptorUrl':
-                ''.join([
-                    'https://$(ref.', cluster_name, '.endpoint)/swaggerapi/',
-                    endpoint
-                ])
-        }
-    })
-    outputs.append({
-        'name': 'clusterType' + type_suffix,
-        'value': type_name + type_suffix
-    })
+  resources.append({
+      'name': type_name,
+      'type': 'deploymentmanager.v2beta.typeProvider',
+      'properties': {
+          'options': {
+              'validationOptions': {
+                  # Kubernetes API accepts ints, in fields they annotate
+                  # with string. This validation will show as warning
+                  # rather than failure for Deployment Manager.
+                  # https://github.com/kubernetes/kubernetes/issues/2971
+                  'schemaValidation': 'IGNORE_WITH_WARNINGS'
+              },
+              # According to kubernetes spec, the path parameter 'name'
+              # should be the value inside the metadata field
+              # https://github.com/kubernetes/community/blob/master
+              # /contributors/devel/api-conventions.md
+              # This mapping specifies that
+              'inputMappings': [{
+                  'fieldName': 'name',
+                  'location': 'PATH',
+                  'methodMatch': '^(GET|DELETE|PUT|POST|PATCH)$',
+                  'value': '$.ifNull('
+                           '$.resource.properties.metadata.name, '
+                           '$.resource.name)'
+              }, {
+                  'fieldName': 'metadata.name',
+                  'location': 'BODY',
+                  'methodMatch': '^(PUT|POST)$',
+                  'value': '$.ifNull('
+                           '$.resource.properties.metadata.name, '
+                           '$.resource.name)'
+              }, {
+                  'fieldName': 'Authorization',
+                  'location': 'HEADER',
+                  'value': '$.concat("Bearer ",'
+                           '$.googleOauth2AccessToken())'
+              }, {
+                  'fieldName': 'metadata.resourceVersion',
+                  'location': 'BODY',
+                  'methodMatch': '^(PUT|PATCH)$',
+                  'value': '$.resource.self.metadata.resourceVersion'
+              }, {
+                  'fieldName': 'id',
+                  'location': 'PATH',
+                  'methodMatch': '^(GET|DELETE|PUT|POST|PATCH)$',
+                  'value': '$.resource.properties.id'
+              }, {
+                  'fieldName': 'namespace',
+                  'location': 'PATH',
+                  'methodMatch': '^(GET|DELETE|PUT|POST|PATCH)$',
+                  'value': '$.resource.properties.namespace'
+              }]
+          },
+          'descriptorUrl':
+              ''.join([
+                  'https://$(ref.', cluster_name, '.endpoint)/openapi/v2'
+              ])
+      }
+  })
 
-  return {'resources': resources, 'outputs': outputs}
+  return {'resources': resources}
